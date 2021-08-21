d29cd51c157d1ce0203bca818ac62689c0a103d4
Rewe <kewyn.ryan@gmail.com>
2021-08-21 16:25:31 -0300

    Added more necessary colors

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,24 +1,35 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
+const theme = {
+  colors: {
+    primary: '#23A6F0',
+    textPrimary: '#252B42',
+    textSecondary: '#737373',
+    lightGray1: '#FAFAFA',
+    success: '#2DC071',
+    muted: '#BDBDBD',
+    disabledElement: '#23A6F0',
+    alert: '#E77C40',
+    textLight: '#FFFFFF'
+  },
+}
+
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    color: ${theme.colors.textSecondary};
+    background-color: ${theme.colors.lightGray1};
+    font-family: 'montserrat';
   }
 `
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-    text: '#252B42',
-  },
-}
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&display=swap" rel="stylesheet"/>
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
